840e964afaa578f604c796817520663b6e9cf377
Arnis Jaundzeikars <sangsatori@users.noreply.github.com>
2017-01-31 14:28:10 +0000

    Refactor code organnization

--- sketch.js
+++ sketch.js
@@ -1,95 +1,118 @@
-let video, palette;
-let box = {
-  size: {
-    x: 3,
-    y: 9
-  }
+const config = {
+  box: {
+    size: {
+      x: 3,
+      y: 9
+    }
+  },
+  palettes: [
+    [
+     "#000000", // black
+     "#555555", // gray
+     "#0000AA", // blue
+     "#5555FF", // light blue
+     "#00AA00", // green
+     "#55FF55", // light green
+     "#00AAAA", // cyan
+     "#55FFFF", // light cyan
+     "#AA0000", // red
+     "#FF5555", // light red
+     "#AA00AA", // magenta
+     "#FF55FF", // light magenta
+     "#AA5500", // brown
+     "#FFFF55", // yellow
+     "#AAAAAA", // light gray
+     "#FFFFFF" // white (high intensity)
+    ]
+  ]
 };
 
+
+let video, palette;
 
 function setup() {
   createCanvas(640, 640);
   background(25);
 
   noStroke();
-
   pixelDensity(1);
 
   video = createCapture();
   video.hide();
   video.volume(0); // mute camera audio
 
-  palette = colors.map(hex => color(hex));
+  // turn hex values into p5.color objects
+  palette = config.palettes[0].map(hex => color(hex));
 }
 
 function draw() {
-  video.loadPixels();
-  let total = box.size.x * box.size.y;
-
   // mirror the image
   push();
   translate(video.width, 0);
   scale(-1.0, 1.0);
 
-  // room for a nice abstraction here
-  for (let x = 0; x < video.width; x += box.size.x) {
-    for (let y = 0; y < video.height; y += box.size.y) {
+  // process pixel values
+  video.loadPixels();
+  const binningArea = config.box.size.x * config.box.size.y;
 
-      let c = {
-        r: 0,
-        g: 0,
-        b: 0
-      };
+  // draw from average colours
+  binArea(video, config.box.size).forEach(area => {
+    fill(...from24to8bit(
+      area.r,
+      area.g,
+      area.b
+    ));
+    rect(area.x, area.y, config.box.size.x, config.box.size.y);
+  });
 
-      for (let i = 0; i < box.size.x; i++) {
-        for (let j = 0; j < box.size.y; j++) {
-          let index = ((x + i) + (y + j) * video.width) * 4;
-          c.r += video.pixels[index + 0];
-          c.g += video.pixels[index + 1];
-          c.b += video.pixels[index + 2];
-        }
-      }
-
-      fill(...to8Bit(
-        (c.r / total),
-        (c.g / total),
-        (c.b / total)
-      ));
-      rect(x, y, box.size.x, box.size.y);
-    }
-  }
   pop();
 }
 
+
+/* Utilities */
+// hoisting floats definitions to top
+
+// change the bit depth; assumes 8-bit input value
 const toBitDepth = (bits, value) => {
   const depth = pow(2, bits);
   const step = pow(2, 8) / depth;
   return Math.floor(value * 7 / 255) * step;
 };
 
-const to8Bit = (r, g, b) => [
+// reduce 24-bit RGB to 8-bit RGB
+const from24to8bit = (r, g, b) => [
   toBitDepth(3, r),
   toBitDepth(3, g),
   toBitDepth(3, b)
 ];
 
-var colors = [
- "#000000", //black
- "#555555", // gray
- "#0000AA", // blue
- "#5555FF", // light blue
- "#00AA00", // green
- "#55FF55", // light green
- "#00AAAA", // cyan
- "#55FFFF", // light cyan
- "#AA0000", // red
- "#FF5555", // light red
- "#AA00AA", // magenta
- "#FF55FF", // light magenta
- "#AA5500", // brown // #AA5500
- "#FFFF55", // yellow
- "#AAAAAA", // light gray
- "#FFFFFF" // white (high intensity)
-];
+// perform average binning on a graphics object
+const binArea = (gfx, size) => {
+  const areaSize = size.x * size.y;
 
-// const to4bit()
+  let results = [];
+  let index, total, coords;
+
+  // traverse graphics
+  for (let x = 0; x < gfx.width; x += size.x) {
+    for (let y = 0; y < gfx.height; y += size.y) {
+      total = { r: 0, g: 0, b: 0 };
+      for (let binX = x; binX < x + size.x; binX++) {
+        for (let binY = y; binY < y + size.y; binY++) {
+          index = (binX + binY * gfx.width) * 4;
+          total.r += gfx.pixels[index + 0];
+          total.g += gfx.pixels[index + 1];
+          total.b += gfx.pixels[index + 2];
+        }
+      }
+      results.push({
+        r: floor(total.r / areaSize),
+        g: floor(total.g / areaSize),
+        b: floor(total.b / areaSize),
+        x, y
+      });
+    }
+  }
+
+  return results;
+};
